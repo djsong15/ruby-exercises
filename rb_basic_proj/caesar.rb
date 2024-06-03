833c4bc4efd9bd9bc02a68533b17d292b39a09aa
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-06-03 07:13:37 -0700

    Complet caesar cipher

--- rb_basic_proj/caesar.rb
+++ rb_basic_proj/caesar.rb
@@ -1,3 +1,20 @@
 def caesar_cipher(str, shift)
-  
+  res = ''
+  str.split('').each do |char|
+    if char.match?(/[[:alpha:]]/)
+      curCharNum = char.downcase.ord
+      newCharNum = curCharNum + shift
+      newCharNum -= 'z'.ord - curCharNum < shift ? 26 : 0
+      if char.ord <= 'Z'.ord
+        res += newCharNum.chr.upcase
+      else
+        res += newCharNum.chr
+      end
+    else
+      res += char
+    end
+  end
+  res
 end
+
+p caesar_cipher("What a string!", 5)
