71447ca787b5301e934fe4631b6938a43a530e54
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-06-04 07:33:30 -0700

    Obey rubocop

--- rb_basic_proj/substring.rb
+++ rb_basic_proj/substring.rb
@@ -6,12 +6,11 @@ def substrings(string, dict)
     str_i = 0
     while str_i < str.length
       if str[str_i] == substr[sub_i]
-        str_i += 1
         sub_i += 1
       else
-        str_i += 1
         sub_i = 0
       end
+      str_i += 1
       if sub_i == substr.length
         res[substr] += 1
         sub_i = 0
