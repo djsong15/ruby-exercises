4d2e3fa1b4a3f41a3032f49a57748840cc16db67
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-06-03 21:33:31 -0700

    Pass first testcase

---
+++ rb_basic_proj/substring.rb
@@ -0,0 +1,21 @@
+def substrings(string, dict)
+  res = Hash.new(0)
+  str = string.downcase.strip
+  dict.each do |substr|
+    sub_i, str_i = 0, 0
+    while str_i < str.length
+      if str[str_i] == substr[sub_i]
+        str_i += 1
+        sub_i += 1
+      else
+        str_i += 1
+      end
+    end
+    sub_i == substr.length && res[substr] += 1
+  end
+  res
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+p substrings("below", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
