d8a30b0f9f9543dee96d84c23b81d6ca0c18e057
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-06-04 06:55:24 -0700

    Complete bubble sort

---
+++ rb_basic_proj/bubble_sort.rb
@@ -0,0 +1,18 @@
+def bubble_sort(arr)
+  i = 1
+  sorted = false
+  until sorted == true do
+    sorted = true
+    while i < arr.length
+      if arr[i] < arr[i - 1]
+        arr[i - 1], arr[i] = arr[i], arr[i - 1]
+        sorted = false
+      end
+      i += 1
+    end
+    i = 1
+  end
+  arr
+end
+
+p bubble_sort([4,3,78,2,0,2])
